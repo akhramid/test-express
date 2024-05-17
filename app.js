6df07f52e81b8f9bb5848b7c92dcfd8c4c8e3bcc
akhramid <aramdhan.pamuji@gmail.com>
2024-05-17 16:31:58 +0700

    💬 change text

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("App is Running"));
-app.get("/ping", (req, res) => res.send("Pong"));
+app.get("/ping", (req, res) => res.send("Pong Pong"));
 app.get("/pong", (req, res) => res.send("Ping Pong"));
 
 module.exports = app;
