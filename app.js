e1a5cb566d255b44f67ff7958562e389ffebb763
akhramid <aramdhan.pamuji@gmail.com>
2024-05-17 11:15:11 +0700

    🚧 add new routes

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => res.send("App is Running"));
+app.get("/ping", (req, res) => res.send("Pong"));
+app.get("/pong", (req, res) => res.send("Ping Pong"));
+
 app.listen(port, (error) => {
-    if(!error){
-        console.log("Server is Running");
-    }else{
-        console.log("Server is Error");
-    }
+  if (!error) {
+    console.log(`Server is Running on ${port}`);
+  } else {
+    console.log("Server is Error");
+  }
 });
